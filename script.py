ca5f75feade041b3e62af2ffde00fb1d605b98e7
brespit <flaviorcleite@gmail.com>
2020-02-09 08:52:04 +0100

    upd folder

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-file_object = open('sample.txt', 'a')
+file_object = open('/opt/python/test/sample.txt', 'a')
 while True:
     now = datetime.now()
     dateString = now.strftime("%m/%d/%Y, %H:%M:%S ")
