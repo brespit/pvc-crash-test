e2785038c80dab48f1276fcfb914dd0f683bcdee
brespit <flaviorcleite@gmail.com>
2020-02-09 08:43:58 +0100

    Test

---
+++ script.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+file_object = open('sample.txt', 'a')
+while True:
+    now = datetime.now()
+    dateString = now.strftime("%m/%d/%Y, %H:%M:%S ")
+    file_object.write(dateString)
+ 
+file_object.close()
